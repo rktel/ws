887c2eb21fd4a529d4a54c2c6068cbed56ef87e8
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-03 15:50:46 -0500

    Volvo Collection 5

--- server/main.js
+++ server/main.js
@@ -16,8 +16,8 @@ import { Volvo } from '../imports/api/collections'
 
 // Listen to incoming HTTP requests (can only be used on the server).
 WebApp.connectHandlers.use('/volvo', (req, res, next) => {
-    res.writeHead(300);
+    res.writeHead(200);
     let volvoRes = Volvo.findOne()
-
+    console.log(req);
     res.end(JSON.stringify(volvoRes));
 });
