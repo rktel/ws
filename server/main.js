62729388be7542a7109229e0eee716d9921082b4
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-05 11:41:26 -0500

    test headers 5

--- server/main.js
+++ server/main.js
@@ -16,12 +16,27 @@ import { Volvo } from '../imports/api/collections'
 
 // Listen to incoming HTTP requests (can only be used on the server).
 WebApp.connectHandlers.use('/volvo', (req, res, next) => {
-    res.writeHead(200);
-    console.log(req.headers);
-    console.log(req.headers.authorization);
-    
-    
-    const atob = str => Buffer.from(str, 'base64').toString('binary');
-    console.log(atob(req.headers.authorization.split(' ')[1]));
-    
+  res.writeHead(200);
+  console.log(getCredentials(req));
+
+
 });
+
+// helper functions
+
+const atob = str => Buffer.from(str, 'base64').toString('binary');
+const getCredentials = str => {
+  if (str && str.headers.authorization && str.headers.authorization.split(' ')[1]) {
+    let credentials = atob(str.headers.authorization.split(' ')[1])
+    let arrCredentials = credentials ? credentials.split(':') : [null, null]
+    return {
+      username: arrCredentials[0],
+      password: arrCredentials[1]
+    }
+  } else {
+    return {
+      username: null,
+      password: null
+    }
+  }
+}
