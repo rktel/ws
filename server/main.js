4d29a243d5f745221db8fa9218557e18b498a97e
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-05 14:10:25 -0500

    test headers 8

--- server/main.js
+++ server/main.js
@@ -13,20 +13,26 @@ app.listen(4100, function () {
 */
 
 import { Volvo } from '../imports/api/collections'
-
+//  400	Bad Request
+// 401	Unauthorized
 // Listen to incoming HTTP requests (can only be used on the server).
 WebApp.connectHandlers.use('/volvo', (req, res, next) => {
-  res.writeHead(200);
-  console.log(getCredentials(req));
-
-
+  const { username, password } = getCredentials(req)
+  if (username, password) {
+    
+    res.writeHead(200);
+    res.end(`Hello world : ${username}`)
+  }else{
+    res.writeHead(401);
+    res.end(`Unauthorized`)
+  }
 });
 
 // helper functions
 
 const atob = str => Buffer.from(str, 'base64').toString('binary');
 const getCredentials = str => {
-  if (str && str.headers.authorization && str.headers.authorization.split(' ')[0] =='Basic' && str.headers.authorization.split(' ')[1]) {
+  if (str && str.headers.authorization && str.headers.authorization.split(' ')[0] == 'Basic' && str.headers.authorization.split(' ')[1]) {
     let credentials = atob(str.headers.authorization.split(' ')[1])
     let arrCredentials = credentials ? credentials.split(':') : [null, null]
     return {
