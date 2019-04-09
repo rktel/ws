0fd168f2843e896eb9c20d78d1527ecb3318d27d
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-09 10:59:34 -0500

    param , query 2

--- server/main.js
+++ server/main.js
@@ -22,10 +22,12 @@ WebApp.connectHandlers.use('/api/1.0', (req, res, next) => {
   if (username == 'volvo' && password == 'vlv_scrts_04_2019') {
 
     res.writeHead(200);
-    console.log(req.param);
     console.log(req.query);
-    
-    res.end(`Hello world : ${username}`)
+    const queries = Object.keys(req.query).length > 0? req.query : false
+    if(queries){
+      console.log('hello');
+      
+    }
     console.log(req.query);
   } else {
     res.writeHead(401);
