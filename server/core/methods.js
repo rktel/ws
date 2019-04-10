c52834e8ca447703cab734850449c1bc6d15725f
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-10 12:33:01 -0500

    queries if 30

--- server/core/methods.js
+++ server/core/methods.js
@@ -10,7 +10,7 @@ Meteor.methods({
         // db.products.find( { description: { $regex: /S/ } } )
         console.log(str);
 
-        const plate = await Volvo.find({ 'events.vehicle': new RegExp(str, 'i') }, {  limit: 1, sort: { _id: 1 } }).fetch()
+        const plate = await Volvo.find({ 'events.vehicle': new RegExp(str, 'i') }, {  sort: { 'events.created':-1 } ,limit: 1}).fetch()
 
         console.log(plate);
         
